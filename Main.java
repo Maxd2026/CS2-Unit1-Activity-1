43627cdd1243edc1d14c90d8f041181613fa42e2
Maxd2026 <maxwell.dolan2026@gbwl.org>
2025-09-18 18:21:41 +0000

    12

--- Main.java
+++ Main.java
@@ -14,5 +14,18 @@ public class Main {
       System.out.println("Type your name then press enter:");
       String player = input.nextLine();
       System.out.println("Welcome," + player);
+
+      // Create variables for each placeholder word
+      String person = "dog";
+      String noun = "shower";
+      String looks = "look-chopped";
+      String emotion = "upset";
+      String item = "wallet";
+      String emotions = "frivilous";
+
+      System.out.println("Today is a very important day because my " + person + " is getting a " + noun);
+      System.out.println("If he gets a bad haircut it will make him " + looks);
+      System.out.println("My dad is also very" + emotion + " because he lost his " + item );
+      System.out.println("I woke up feeling very" + emotions);
    }
 }
